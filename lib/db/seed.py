ab8f8c2c93a76e32b1f822bed60b8f1b737efe0a
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-15 14:20:32 +1000

    created seed table function

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -3,20 +3,26 @@ import csv
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 from models import Driver, engine
-
+from helpers import get_csv
 Session = sessionmaker(bind=engine)
-csv_file_path = "csv/drivers.csv"
 
-with open(csv_file_path, 'r') as file:
-    csv_data = csv.DictReader(file)
 
-    # Convert the data to a list and process the 'dob' values
-    data = []
-    for row in csv_data:
-        row['dob'] = datetime.strptime(row['dob'], '%Y-%m-%d').date()
-        data.append(row)
+"drivers.csv"
 
-    session = Session()
-    session.bulk_insert_mappings(Driver, data)
-    session.commit()
-    session.close()
+
+def seed_tables(table_name, model):
+    csv_file_path = get_csv(table_name)
+
+    with open(csv_file_path, 'r') as file:
+        csv_data = csv.DictReader(file)
+
+        # Convert the data to a list and process the 'dob' values
+        data = []
+        for row in csv_data:
+            instance = model(**row)
+            data.append(instance)
+
+        session = Session()
+        session.bulk_insert_mappings(Driver, data)
+        session.commit()
+        session.close()
