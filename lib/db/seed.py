efbaa41e3899098c55928b0b5b27f741a41ce1d3
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-20 05:14:31 +1000

    created different seed functions for each table

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -2,17 +2,20 @@ import os
 import csv
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
-from models import Race, engine
+from models import Race, engine, Status, Season, Driver, Constructor, Circuit
 
 Session = sessionmaker(bind=engine)
+
+table = "races.csv"
+model = Race
 
 
 def get_csv(csv_name):
     return f"csv/{csv_name}"
 
 
-def seed_tables(table_name, model):
-    csv_file_path = get_csv(table_name)
+def seed_races(table, model):
+    csv_file_path = get_csv(table)
     with open(csv_file_path, 'r') as file:
         csv_data = csv.DictReader(file)
         data = []
@@ -47,6 +50,109 @@ def seed_tables(table_name, model):
     session.close()
 
 
-table_name = "races.csv"
-model = Race
-seed_tables(table_name, model)
+def seed_status(table, model):
+    csv_file_path = get_csv(table)
+    with open(csv_file_path, 'r') as file:
+        csv_data = csv.DictReader(file)
+        data = []
+        for row in csv_data:
+            statusId = int(row['statusId']),
+            status = row['status']
+
+            instance = model(**row)
+            data.append(instance)
+
+    session = Session()
+    session.add_all(data)
+    session.commit()
+    session.close()
+
+
+def seed_constructors(table, model):
+    csv_file_path = get_csv(table)
+    with open(csv_file_path, 'r') as file:
+        csv_data = csv.DictReader(file)
+        data = []
+        for row in csv_data:
+            constructorId = int(row['constructorId']),
+            constructorRef = row['constructorRef'],
+            name = row['name'],
+            nationality = row['nationality'],
+            url = row['url']
+
+            instance = model(**row)
+            data.append(instance)
+
+    session = Session()
+    session.add_all(data)
+    session.commit()
+    session.close()
+
+
+def seed_circuits(table, model):
+    csv_file_path = get_csv(table)
+    with open(csv_file_path, 'r') as file:
+        csv_data = csv.DictReader(file)
+        data = []
+        for row in csv_data:
+            circuitId = int(row['circuitId']),
+            circuitRef = row['circuitRef'],
+            name = row['name'],
+            location = row['location'],
+            country = row['country'],
+            lat = float(row['lat']),
+            lng = float(row['lng']),
+            alt = int(row['alt']),
+            url = row['url']
+
+            instance = model(**row)
+            data.append(instance)
+
+    session = Session()
+    session.add_all(data)
+    session.commit()
+    session.close()
+
+
+def seed_seasons(table, model):
+    csv_file_path = get_csv(table)
+    with open(csv_file_path, 'r') as file:
+        csv_data = csv.DictReader(file)
+        data = []
+        for row in csv_data:
+            row['year'] = int(row['year'])
+            url = row['url']
+
+            instance = model(**row)
+            data.append(instance)
+
+    session = Session()
+    session.add_all(data)
+    session.commit()
+    session.close()
+
+
+def seed_drivers(table, model):
+    csv_file_path = get_csv(table)
+    with open(csv_file_path, 'r') as file:
+        csv_data = csv.DictReader(file)
+        data = []
+        for row in csv_data:
+            driverId = int(row['driverId']),
+            driverRef = row['driverRef'],
+            number = row['number'],
+            code = row['code'],
+            forename = row['forename'],
+            surname = row['surname'],
+            # Convert to date object
+            dob = datetime.strptime(row['dob'], '%Y-%m-%d').date(),
+            nationality = row['nationality'],
+            url = row['url']
+
+            instance = model(**row)
+            data.append(instance)
+
+    session = Session()
+    session.add_all(data)
+    session.commit()
+    session.close()
